50991a7556de9d548eca480822c7937aea60b8cc
Ryan Shaw <ryanshaw@ischool.berkeley.edu>
2011-02-02 14:57:11 -0500

    Added comments to Atom feed.

--- mysite/blog/feeds.py
+++ mysite/blog/feeds.py
@@ -1,4 +1,5 @@
 from django.contrib.syndication.views import Feed, FeedDoesNotExist
+from django.contrib.comments.models import Comment
 from django.utils.feedgenerator import Atom1Feed
 from django.shortcuts import get_object_or_404
 from django.utils.encoding import force_unicode
@@ -23,16 +24,36 @@ class BlogFeed(Feed):
         return 'Course blog for %s' % blog.title
 
     def items(self, blog):
-        return blog.posts.filter(published=True)[:10]
+        items = []
+        for post in blog.posts.filter(published=True)[:10]:
+            items.append(post)
+            for comment in Comment.objects.for_model(post):
+                items.append(comment)
+        return items
 
-    def item_title(self, post):
-        return post.title
+    def item_title(self, item):
+        if hasattr(item, 'title'):
+            return item.title
+        else:
+            return 'Comment on %s' % item.content_object.title
 
-    def item_description(self, post):
-        return mark_safe(markdown2.markdown(force_unicode(post.content)))
+    def item_description(self, item):
+        if hasattr(item, 'content'):
+            content = item.content
+        else:
+            content = item.comment
+        return mark_safe(markdown2.markdown(force_unicode(content)))
 
-    def item_author_name(self, post):
-        return post.display_name
+    def item_author_name(self, item):
+        if hasattr(item, 'display_name'):
+            return item.display_name
+        elif item.user_name == 'anonymous':
+            return ''
+        else:
+            return item.name
 
-    def item_pubdate(self, post):
-        return post.updated_at
+    def item_pubdate(self, item):
+        if hasattr(item, 'updated_at'):
+            return item.updated_at
+        else:
+            return item.submit_date
