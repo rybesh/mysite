91998838c542f4c977d1cc45b5d1ce83e9dfd3a0
Ryan Shaw <ryanshaw@unc.edu>
2011-08-21 21:49:58 -0400

    Use Request Context for 500 page; courses/ redirects to /teaching.

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.views.generic.simple import redirect_to, direct_to_template
+from django.template import RequestContext, loader
+from django import http
 
 admin.autodiscover()
 
@@ -13,6 +15,11 @@ def loggedin(request):
     for course in request.user.courses.all()[:1]:
         return redirect(course)
     return redirect('/')
+
+def server_error(request, template_name='500.html'):
+    t = loader.get_template(template_name)
+    return http.HttpResponseServerError(t.render(RequestContext(request, {})))
+handler500 = server_error 
 
 urlpatterns = patterns('',
     url(r'^deploy/$', 'mysite.shared.views.deploy', name='shared_deploy_view'),
@@ -28,6 +35,8 @@ urlpatterns = patterns('',
     (r'^reading/', include('mysite.reading.urls')),
     (r'^talking/', include('mysite.talking.urls')),
     (r'^teaching/', include('mysite.courses.urls')),
+    (r'^courses/(?P<path>.*)$', redirect_to, {'url': '/teaching/%(path)s'}),
+     
 
     url(r'^comments/post/$', 'mysite.comments.views.post_comment', 
         name='comments_post_comment_view'),
@@ -38,3 +47,5 @@ urlpatterns = patterns('',
     (r'^loggedin/$', loggedin),
     (r'', include('django.contrib.auth.urls')),
 )
+
+
