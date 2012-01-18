01adbf46dd5d5ccb66e8389bb784df7c066e9cbd
Ryan Shaw <ryanshaw@unc.edu>
2012-01-18 00:06:41 -0500

    Various course management enhancements.

--- mysite/courses/management/commands/addstudents.py
+++ mysite/courses/management/commands/addstudents.py
@@ -3,22 +3,74 @@ from django.core.management.base import CommandError
 from django.db import transaction
 from mysite.courses.models import Course
 from utils import MyBaseCommand
+from HTMLParser import HTMLParser
 import csv
 
+def parse_csv(filename):
+    for row in csv.DictReader(open(filename, 'rb')):
+        last_name, first_name = row['Name'].split(',')
+        email = row['Email']
+        yield (lastname, first_name, email)
+
+class TableReader(HTMLParser):
+    def read(self, f):
+        self.tag = None
+        self.data = ''
+        self.keys = []
+        self.rows = []
+        self.keyindex = 0
+        for line in f:
+            self.feed(line.strip())
+        return self.rows
+    def handle_starttag(self, tag, attrs):
+        self.tag = tag
+        if tag == 'tr':
+            if len(self.keys) > 0:
+                self.rows.append({})
+                self.keyindex = 0
+    def handle_endtag(self, tag):
+        if self.tag == 'th':
+            self.keys.append(self.data)
+        elif self.tag == 'td':
+            key = self.keys[self.keyindex]
+            self.rows[-1][key] = self.data
+            self.keyindex += 1
+        self.tag = None
+        self.data = ''
+    def handle_data(self, data):
+        self.data += data
+
 class Command(MyBaseCommand):
-    args = '<csv_filename>'
+    args = '<student_info_file> <emails_file>'
     help = 'Creates student accounts and adds them to a course.'
     @transaction.commit_on_success
     def handle(self, *args, **options):
-        if not len(args) == 1:
+        if not len(args) == 2:
             raise CommandError(
-                'You must provide the name of (just) one CSV file with student info.')
+                'You must provide the names of files with student info and emails.')
+        if args[0].endswith('.csv'):
+            reader = csv.DictReader
+        elif args[0].endswith('.xls'):
+            reader = TableReader().read
+        else:
+            raise CommandError(
+                'Student info must be in CSV or XLS (really HTML).')
+        emails = []
+        with open(args[1]) as f:
+            for line in f:
+                emails.append(line.strip())
         students = []
         new_count = existing_count = 0
-        for row in csv.DictReader(open(args[0], 'rb')):
+        rows = reader(open(args[0], 'rb'))
+        if not len(emails) == len(rows):
+            raise CommandError(
+                'Number of emails does not match number of students.')
+        for i, row in enumerate(rows):
             try:
                 last_name, first_name = row['Name'].split(',')
-                email = row['Email']
+                email = emails[i]
+                if email.endswith('@email.unc.edu'):
+                    email = email.replace('@email.unc.edu', '@live.unc.edu')
                 username = first_name[0].lower() + last_name.lower()
                 student, created = User.objects.get_or_create(
                     first_name=first_name, last_name=last_name,
@@ -28,6 +80,7 @@ class Command(MyBaseCommand):
                 else:
                     existing_count += 1
                 student.email = email
+                student.is_active = True
                 student.save()
                 students.append(student)
             except KeyError as e:
@@ -36,6 +89,9 @@ class Command(MyBaseCommand):
         self.stdout.write('To which course will these students be added?\n')
         course = self.input_course()
         added_count = existing_count = 0
+        for student in course.students.all():
+            if not student in students:
+                self.stdout.write('%s appears to have dropped the course.\n' % student.email)
         for student in students:
             if course.has_student(student):
                 existing_count += 1
