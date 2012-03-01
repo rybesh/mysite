df389000137492a20b3921dede8f99ab06cb4408
Ryan Shaw <ryanshaw@unc.edu>
2012-03-01 12:20:02 -0500

    Serve .doc files.

--- mysite/files/urls.py
+++ mysite/files/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<path>.+(\.pdf|\.zip|\.html))$', 
+    url(r'^(?P<path>.+(\.pdf|\.zip|\.html|\.doc))$', 
         views.sendfile, name='files_sendfile_view'),
 )
