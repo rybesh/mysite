82c78d7809d5de8f8a0f613c5e2e58824079f7fa
Ryan Shaw <ryanshaw@unc.edu>
2012-08-28 14:11:57 -0400

    Add docx to sendfile view.

--- mysite/files/urls.py
+++ mysite/files/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<path>.+(\.pdf|\.zip|\.html|\.doc))$', 
+    url(r'^(?P<path>.+(\.pdf|\.zip|\.html|\.doc|\.docx))$', 
         views.sendfile, name='files_sendfile_view'),
 )
